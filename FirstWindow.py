5d16b8f21baabd3521007977729fd308882b155f
wrains54 <wilmo2002@gmail.com>
2024-04-06 09:26:24 -0500

    created database and put data in the database

--- FirstWindow.py
+++ FirstWindow.py
@@ -9,7 +9,7 @@ class CategorySelectionWindow:
         self.label = ttk.Label(master, text="Please select a category:")
         self.label.pack()
 
-        self.categories = ["Category 1", "Category 2", "Category 3"]
+        self.categories = ["Adv Finance", "Python", "Computer Forensics", "DataBase"]
         self.selected_category = tk.StringVar()
         self.category_selector = ttk.Combobox(master, values=self.categories, textvariable=self.selected_category)
         self.category_selector.pack()
